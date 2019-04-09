59201c1ca85edf13359658b325f0c6b48047fe30
shampansky <y.shadrin@gmail.com>
2019-04-09 08:42:32 +0300

    Настроены плагины и сборка проекта

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore")
+var server = require("browser-sync").create();
+var del = require("del");
+var imagemin = require("gulp-imagemin");
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("sprite", function () {
+  return gulp.src(["source/img/icon-*.svg"])
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+})
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff, woff2}",
+    // "source/img/**",
+    "source/js/**",
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("serve", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/*.html", gulp.series("html"));
+  gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "images", "html"));
