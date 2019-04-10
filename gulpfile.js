fe012c106592514891a3cb7b6fff464692c09d77
shampansky <y.shadrin@gmail.com>
2019-04-10 10:31:36 +0300

    исправил ошибку с копированием шрифтов woff2 в сборку

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,8 @@ gulp.task("html", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff, woff2}",
+    "source/fonts/**/*.woff",
+    "source/fonts/**/*.woff2",
     // "source/img/**",
     "source/js/**",
   ], {
